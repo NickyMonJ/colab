3830f798a85b6f69139a44c402e54f422699848f
NickyMonJ Monje <nickymonje@gmail.com>
2023-07-13 02:25:13 +0000

    Boton subscripcion creado

--- colabapp/academy/urls.py
+++ colabapp/academy/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import v_course
+from .views import v_subscribe
 
 urlpatterns = [
-    path('course/<int:course_id>', v_course)
+    path('course/<int:course_id>', v_course),
+    path('course/<int:course_id>/subscribe', v_subscribe)
 ]
